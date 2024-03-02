e3f2311b6fb0fd8a7beb4f216c8c9815f9be867b
nico-slk <nico1991.6@gmail.com>
2024-03-02 05:04:46 -0300

    trabajo agenda finalizado

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -9,7 +9,7 @@ export const ContactCard = ({ contactId, contact, deleteContactFn }) => {
           <p>ID: </p><p className='contact-info'>{contact.id || 0}</p>
         </div>
         <div id='card-namelastname' className='card-section'>
-          <p>Nombre y apellido: </p><p className='contact-info'>{contact.name} {contact.lastname}</p>
+          <p>Nombre y apellido: </p><p className='contact-info'>{contact.name} {contact.lastName}</p>
         </div>
       </div>
       <div id='card-emailnumber' className='card-section'>
