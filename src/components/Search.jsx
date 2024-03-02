3479518a7b7e1f6540cc646050ce104f5370c129
nico-slk <nico1991.6@gmail.com>
2024-03-02 05:12:30 -0300

    trabajo agenda finalizado

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,3 +1,4 @@
+import { IconContext } from 'react-icons';
 import { FaSearch } from 'react-icons/fa';
 import './search.css';
 
@@ -6,7 +7,11 @@ export const Search = ({ handleSearch }) => {
   return (
     <div className='search-container'>
       <input type="text" className='search-bar' placeholder='Buscar contacto' onChange={handleSearch} />
-      <FaSearch />
+      <IconContext.Provider value={{ size: "1.5em", className: "global-class-name" }}>
+        <div className='icon'>
+          <FaSearch />
+        </div>
+      </IconContext.Provider>
     </div>
   );
 };
